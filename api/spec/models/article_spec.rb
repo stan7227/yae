7e38a0a78f7b52fbdac523ea03f68d5acecfc59b
stan <nocchi227@yahoo.co.jp>
2021-12-28 21:32:13 +0900

    rubocop で指摘された場所を修正

--- api/spec/models/article_spec.rb
+++ api/spec/models/article_spec.rb
@@ -29,12 +29,14 @@ RSpec.describe "Api::V1::Articles", type: :request do
 
     it "記事の一覧が取得できる" do
       subject
-      res = JSON.parse(response.body)
+      aggregate_failures "最後まで通過" do
+        res = JSON.parse(response.body)
 
-      expect(response).to have_http_status(:ok)
-      expect(res.length).to eq 3
-      expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
-      expect(res[0]["user"].keys).to eq ["id", "name", "email"]
+        expect(response).to have_http_status(:ok)
+        expect(res.length).to eq 3
+        expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
+        expect(res[0]["user"].keys).to eq ["id", "name", "email"]
+      end
     end
   end
 end
