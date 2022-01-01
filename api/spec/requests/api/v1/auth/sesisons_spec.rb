fa8747f94ea90f58fc9537c1a9ee26db6c5e98e3
stan <nocchi227@yahoo.co.jp>
2022-01-01 16:13:09 +0900

    ログアウトに関するテストを実装

--- api/spec/requests/api/v1/auth/sesisons_spec.rb
+++ api/spec/requests/api/v1/auth/sesisons_spec.rb
@@ -48,4 +48,31 @@ RSpec.describe "Api::V1::Auth::Sessions", type: :request do
       end
     end
   end
+
+  describe "DELETE /api/v1/auth/sign_out" do
+    subject { delete(destroy_api_v1_user_session_path, headers: headers) }
+
+    context "ログアウトに必要な情報を送信したとき" do
+      let(:user) { create(:user) }
+      let!(:headers) { user.create_new_auth_token }
+
+      it "ログアウトできる" do
+        subject
+        expect(user.reload.tokens).to be_blank
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "謝った情報を送信したとき" do
+      let(:user) { create(:user) }
+      let!(:headers) { { "access-token" => "", "token-type" => "", "client" => "", "expiry" => "", "uid" => "" } }
+
+      it "ログアウトできない" do
+        subject
+        expect(response).to have_http_status(:not_found)
+        res = JSON.parse(response.body)
+        expect(res["errors"]).to include "User was not found or was not logged in."
+      end
+    end
+  end
 end
