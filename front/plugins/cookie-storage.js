a5003c3fc2b4814508b350049cab923b3bff7024
stan <nocchi227@yahoo.co.jp>
2022-01-10 02:32:51 +0900

    plugins に設定ファイルを追加

---
+++ front/plugins/cookie-storage.js
@@ -0,0 +1,27 @@
+import createPersistedState from 'vuex-persistedstate'
+import Cookies from 'js-cookie'
+import cookie from 'cookie'
+
+const COOKIE_TARGET_STORE = ['user']
+
+export default ({ store, req, isDev }) => {
+  createPersistedState({
+    key: 'docker_wonderful',
+    paths: COOKIE_TARGET_STORE,
+    storage: {
+      getItem: (key) =>
+        process.client
+          ? Cookies.get(key)
+          : cookie.parse(req.headers.cookie)[key],
+      setItem: (key, value) => {
+        const cookie = Cookies.set(key, value, {
+          expires: 14, // 14日間有効
+          secure: !isDev,
+          sameSite: 'lax',
+        })
+        return cookie
+      },
+      removeItem: (key) => Cookies.remove(key),
+    },
+  })(store)
+}
