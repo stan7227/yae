8c74ece86dd810fdb5c89ef482c059fbb19a3d9c
stan <nocchi227@yahoo.co.jp>
2022-02-26 23:06:56 +0900

    Task model のバリデーションの設定

--- api/app/models/task.rb
+++ api/app/models/task.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Task < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 80 }
+  validates :body, length: { maximum: 100 }
 end
