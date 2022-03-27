9d615adb1fc15a59a69f1fd9b8b85bb2ed65acc3
stan <nocchi227@yahoo.co.jp>
2022-03-27 23:50:53 +0900

    rubocop指摘箇所修正

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -9,9 +9,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins Settings.frontend.url
     if Rails.env.production?
-      origins 'https://static.yaesite.work'
+      origins "https://static.yaesite.work"
     else
-      origins 'http://localhost:8080'
+      origins "http://localhost:8080"
     end
     resource "*",
              headers: :any,
